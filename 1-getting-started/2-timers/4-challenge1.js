9750abb6e0413e3414af4f28f79c9af7f283cddd
HansSimbala <hans31_1998@hotmail.com>
2019-09-07 02:29:46 -0500

    commit by hans

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,12 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
-
+const theOneFunc = (x) => {
+    setTimeout((x) => {
+        console.log('Hello after ',x);
+    }, x * 1000);
+};
 // Hello after 4 seconds
-
 // Hello after 8 seconds
-
+for (var i = 0; i < 8;) {
+    i = i + 4;
+    theOneFunc(i);
+}
 // You can define only ONE function
